3cde693415464b8f57552f13fc8297c61debce78
joao4018 <joao4018@gmail.com>
2020-04-03 12:32:41 -0300

    Modularizando

---
+++ core/src/main/java/com/microsservices/course/endpoint/service/CourseService.java
@@ -0,0 +1,24 @@
+package com.microsservices.course.endpoint.service;
+
+import com.microsservices.course.model.Course;
+import com.microsservices.course.repository.CourseRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author William Suane
+ */
+@Service
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class CourseService {
+    private final CourseRepository courseRepository;
+
+    public Iterable<Course> list(Pageable pageable) {
+        log.info("Listing all courses");
+        return courseRepository.findAll(pageable);
+    }
+}
