0689e41c86d2abce843d0482a6ff9326fe50c090
joao4018 <joao4018@gmail.com>
2020-04-04 03:48:56 -0300

    Criando authenticador

---
+++ auth/src/main/java/com/microsservices/auth/docs/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.microsservices.auth.docs;
+
+
+import com.microsservices.core.docs.BaseSwaggerConfig;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author joao4018
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends BaseSwaggerConfig {
+    public SwaggerConfig() {
+        super("academy.devdojo.youtube.auth.endpoint.controller");
+    }
+}
+
---
+++ auth/src/main/java/com/microsservices/auth/security/config/SecurityCredentialsConfig.java
@@ -0,0 +1,54 @@
+package com.microsservices.auth.security.config;
+
+import com.microsservices.auth.security.filter.JwtUsernameAndPasswordAuthenticationFilter;
+import com.microsservices.core.property.JwtConfiguration;
+import com.microsservices.security.config.SecurityTokenConfig;
+import com.microsservices.security.filter.JwtTokenAuthorizationFilter;
+import com.microsservices.security.token.converter.TokenConverter;
+import com.microsservices.security.token.creator.TokenCreator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * @author joao4018
+ */
+@EnableWebSecurity
+public class SecurityCredentialsConfig extends SecurityTokenConfig {
+    private final UserDetailsService userDetailsService;
+    private final TokenCreator tokenCreator;
+    private final TokenConverter tokenConverter;
+
+    public SecurityCredentialsConfig(JwtConfiguration jwtConfiguration,
+                                     @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService,
+                                     TokenCreator tokenCreator, TokenConverter tokenConverter) {
+        super(jwtConfiguration);
+        this.userDetailsService = userDetailsService;
+        this.tokenCreator = tokenCreator;
+        this.tokenConverter = tokenConverter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfiguration, tokenCreator))
+                .addFilterAfter(new JwtTokenAuthorizationFilter(jwtConfiguration, tokenConverter), UsernamePasswordAuthenticationFilter.class);
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
