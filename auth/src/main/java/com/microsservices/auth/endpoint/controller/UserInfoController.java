75fb7407058f7e22e68992e5510cfac1fdd282c1
joao4018 <joao4018@gmail.com>
2020-04-05 19:27:41 -0300

    criando signup

--- auth/src/main/java/com/microsservices/auth/endpoint/controller/UserInfoController.java
+++ auth/src/main/java/com/microsservices/auth/endpoint/controller/UserInfoController.java
@@ -1,16 +1,19 @@
 package com.microsservices.auth.endpoint.controller;
 
 import com.microsservices.core.model.ApplicationUser;
+import com.microsservices.core.repository.ApplicationUserRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 /**
@@ -21,10 +24,26 @@ import java.security.Principal;
 @Api(value = "Endpoints to manage User's information")
 public class UserInfoController {
 
+    private final ApplicationUserRepository applicationUserRepository;
+
+    public UserInfoController(ApplicationUserRepository userDAO) {
+        this.applicationUserRepository = userDAO;
+    }
+
     @GetMapping(path = "info", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ApiOperation(value = "Will retrieve the information from the user available in the token", response = ApplicationUser.class)
     public ResponseEntity<ApplicationUser> getUserInfo(Principal principal) {
         ApplicationUser applicationUser = (ApplicationUser) ((UsernamePasswordAuthenticationToken) principal).getPrincipal();
         return new ResponseEntity<>(applicationUser, HttpStatus.OK);
     }
+    @PostMapping(path = "signup")
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> save(@Valid @RequestBody ApplicationUser applicationUser) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String password =  passwordEncoder.encode(applicationUser.getPassword());
+        applicationUser.setPassword(password);
+        ApplicationUser userSignup = new ApplicationUser(applicationUser);
+        return new ResponseEntity<>(applicationUserRepository.save(userSignup), HttpStatus.CREATED);
+    }
+
 }
--- core/src/main/java/com/microsservices/core/docs/BaseSwaggerConfig.java
+++ core/src/main/java/com/microsservices/core/docs/BaseSwaggerConfig.java
@@ -2,11 +2,15 @@ package com.microsservices.core.docs;
 
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 /**
  * @author joao4018
@@ -23,7 +27,12 @@ public class BaseSwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(basePackage))
-                .build()
+                .build().globalOperationParameters(Collections.singletonList(new ParameterBuilder()
+                        .name("Authorization")
+                        .description("Bearer token")
+                        .modelRef(new ModelRef("string"))
+                        .parameterType("header")
+                        .build()))
                 .apiInfo(metaData());
     }
 
